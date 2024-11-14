e7976ea5267586b44bf1ef1836e78e103a555f7c
AnderGI <agibarguen@gmail.com>
2024-11-14 17:11:44 +0100

    Using express validator validating put image request

--- src/apps/backoffice/backend/routes/submit-image.route.ts
+++ src/apps/backoffice/backend/routes/submit-image.route.ts
@@ -1,11 +1,24 @@
-import { Request, Response, Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
+import { body, ValidationError, validationResult } from 'express-validator';
+import httpStatus from 'http-status';
 
 import { ImagePutController } from '../controllers/submit-image/ImagePutController';
 import container from '../dependency-injection';
 
 export const register = (router: Router): void => {
 	const controller = container.get<ImagePutController>('apps.backoffice.ImagePutController');
-	router.put('/images/:id', (req: Request, res: Response) => {
+	router.put('/images/:id', body('path').isString(), validateReq, (req: Request, res: Response) => {
 		controller.run(req, res);
 	});
 };
+
+function validateReq(req: Request, res: Response, next: NextFunction) {
+	const result = validationResult(req);
+	if (result.isEmpty()) {
+		next();
+
+		return;
+	}
+	const errors = result.array().map((error: ValidationError) => ({ [error.param]: error }));
+	res.status(httpStatus.UNPROCESSABLE_ENTITY).json(errors);
+}
--- tests/apps/backoffice/backend/features/submit-image.feature
+++ tests/apps/backoffice/backend/features/submit-image.feature
@@ -2,7 +2,7 @@ Feature: Submit Image endpoint
   In order to correlate images to emotions 
   An authenticated user must be able to submit images
 
-  Scenario: Check the submit images endpoint
+  Scenario: Put a valid image
     Given I send a PUT request to "/images/919f1d78-8caa-4311-bdbf-fbb093685fe6" with body:
     """
     {
@@ -11,3 +11,12 @@ Feature: Submit Image endpoint
     """
     Then the response status code should be 201
     Then the response should be empty
+
+  Scenario: Put an invaid image
+    Given I send a PUT request to "/images/919f1d78-8caa-4311-bdbf-fbb093685fe6" with body:
+    """
+    {
+      "path":55
+    }
+    """
+    Then the response status code should be 422
