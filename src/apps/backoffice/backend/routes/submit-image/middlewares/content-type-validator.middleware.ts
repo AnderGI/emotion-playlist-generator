99920a84a58518b7da453a7c17910f4ffe5f7097
AnderGI <agibarguen@gmail.com>
2024-11-24 18:20:40 +0100

    Fix linter errors

--- src/apps/backoffice/backend/routes/submit-image/middlewares/content-type-validator.middleware.ts
+++ src/apps/backoffice/backend/routes/submit-image/middlewares/content-type-validator.middleware.ts
@@ -1,8 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
 
-
-
 export default function validateContentType(req: Request, res: Response, next: NextFunction): void {
 	const contentType = req.headers['content-type'];
 	if (contentType?.includes('multipart/form-data')) {
--- src/contexts/shared/infrastructure/persistence/typeorm/TypeOrmClientFactory.ts
+++ src/contexts/shared/infrastructure/persistence/typeorm/TypeOrmClientFactory.ts
@@ -24,7 +24,7 @@ export class TypeOrmClientFactory {
 		try {
 			return connection.initialize();
 		} catch (error) {
-			console.log(`Error in TypeOrm factory ${error}`);
+			console.log(`Error in TypeOrm factory`);
 			process.exit(1);
 		}
 	}
