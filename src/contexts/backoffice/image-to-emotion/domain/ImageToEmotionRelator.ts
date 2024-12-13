11e8d1099e5c3bd57ee1e942d3d321c93219090b
AnderGI <agibarguen@gmail.com>
2024-12-13 20:11:39 +0100

    Fixed linting issues

--- src/contexts/backoffice/image-to-emotion/domain/ImageToEmotionRelator.ts
+++ src/contexts/backoffice/image-to-emotion/domain/ImageToEmotionRelator.ts
@@ -1,8 +1,8 @@
-import { ImagePath } from '../../image/domain/ImageFilename';
+import { ImageFilename } from '../../image/domain/ImageFilename';
 
 export type GeneratorResult = {
 	emotion: string;
 };
 export default interface ImageToEmotionRelator {
-	relate(path: ImagePath): Promise<GeneratorResult>;
+	relate(path: ImageFilename): Promise<GeneratorResult>;
 }
